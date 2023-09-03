b7f3b69561f38b44a2a2e5b941f2627000d4dc3f
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-09-04 01:09:19 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a = 10;
+   float b = (float) a;
+   cout << b << endl;
+   
+   int c = 5;
+   float d = (float) c;
+   cout << d << endl;
+   
+   char grade = 'A';
+   float score = (int) grade;
+   cout<< score << endl;
+   
+   return 0;
+}
